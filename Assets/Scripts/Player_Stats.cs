bbe3f61cb608c98d18841f16fb4211431e293872
Colin Chen <gotfever23456@gmail.com>
2026-09-18 23:07:08 -0400

    Updated player stats

---
+++ Assets/Scripts/Player_Stats.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+[System.Serializable]
+public class CharacterStats
+{
+
+    public int health =80;
+    public int speed = Random.Range(1,8); //rogues get a buff
+    public int mana = Random.Range(80,100); //mages get a flat 100
+    public int dexterity = Random.Range(1,8); //archer gets a buff
+    public int intellignece = Random.Range(1,2); //mages get a buff
+    public int atk = Random.Range(5,10); // warriors get a buff
+
+
+    
+}
