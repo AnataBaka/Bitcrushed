f4999f6aca5fc73dcd50e3c926c0ae0ff49c8100
Colin Chen <gotfever23456@gmail.com>
2026-09-18 23:11:24 -0400

    Update Player_Stats.cs

--- Assets/Scripts/Player_Stats.cs
+++ Assets/Scripts/Player_Stats.cs
@@ -6,7 +6,9 @@ public class CharacterStats
     public int maxHealth =80;
     public int currHealth = 80;
     public int speed = Random.Range(1,8); //rogues get a buff
-    public int mana = Random.Range(80,100); //mages get a flat 100
+    
+    public int maxMana = Random.Range(80,100); //mages get a flat 100
+    public int mana = maxMana;
     public int dexterity = Random.Range(1,8); //archer gets a buff
     public int intellignece = Random.Range(1,2); //mages get a buff
     public int atk = Random.Range(5,10); // warriors get a buff
