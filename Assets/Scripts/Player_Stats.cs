fdd438c54b3967e8a34455312173181687f7602e
Colin Chen <gotfever23456@gmail.com>
2026-09-18 23:08:14 -0400

    Update Player_Stats.cs

--- Assets/Scripts/Player_Stats.cs
+++ Assets/Scripts/Player_Stats.cs
@@ -9,6 +9,8 @@ public class CharacterStats
     public int dexterity = Random.Range(1,8); //archer gets a buff
     public int intellignece = Random.Range(1,2); //mages get a buff
     public int atk = Random.Range(5,10); // warriors get a buff
+    public int xp = 0;
+    
 
 
     
